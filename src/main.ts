80f841cd370e8aa34083e21d29655c068557b92f
Christoph Stach <christoph.stach@gmail.com>
2020-11-17 20:39:39 +0800

    Add support for heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3334;
+
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest Bookstore Shopping Cart API')
+    .setDescription(
+      'Use these endpoints to add items to the shopping cart.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port, () => {
+    Logger.log(`Listening at http://localhost:${port}/`, 'Bootstrap');
+  });
 }
 bootstrap();
